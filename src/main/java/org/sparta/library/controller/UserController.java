34da3ee18f90ed899407760451afe984d92340c9
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-03 05:02:07 +0900

    [feat] UserController 작성

--- src/main/java/org/sparta/library/controller/UserController.java
+++ src/main/java/org/sparta/library/controller/UserController.java
@@ -1,7 +1,24 @@
 package org.sparta.library.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.sparta.library.dto.userDto.UserRequestDto;
+import org.sparta.library.dto.userDto.UserResponseDto;
+import org.sparta.library.service.userservice.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    // 회원 등록 기능
+    @PostMapping("/user")
+    public UserResponseDto createUser(@RequestBody UserRequestDto userRequestDto) {
+        return userService.createUser(userRequestDto);
+    }
 }
