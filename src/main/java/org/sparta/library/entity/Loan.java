a6496a7f0fe41954aa3c28324a5fe0b5736bfe66
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-03 02:57:10 +0900

    [feat] 대출 엔티티 JPA 기반 어노테이션 및 외래키 설정

--- src/main/java/org/sparta/library/entity/Loan.java
+++ src/main/java/org/sparta/library/entity/Loan.java
@@ -1,19 +1,37 @@
 package org.sparta.library.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
+@Entity
+@Table(name = "loan")
 @NoArgsConstructor
 @Getter
 @Setter
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "loan_id")
     private Long loanId;
-    private Long userId;
-    private Long bookId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", referencedColumnName = "book_id", nullable = false)
+    private Book book;
+
+    @Column(name = "book_return", nullable = false)
     private Boolean bookReturn;
+
+    @Column(name = "loan_date", nullable = false)
     private Date loanDate;
+
+    @Column(name = "return_date", nullable = false)
     private Date returnDate;
 }
