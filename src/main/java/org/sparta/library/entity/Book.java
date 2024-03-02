90cd45d3480f9606b60018726dc95a5e984fb81d
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-02 22:55:48 +0900

    [feat] 도서 엔티티 필드 선언

--- src/main/java/org/sparta/library/entity/Book.java
+++ src/main/java/org/sparta/library/entity/Book.java
@@ -1,4 +1,19 @@
 package org.sparta.library.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Book {
+    private Long bookId;
+    private String title;
+    private String writer;
+    private String language;
+    private String publisher;
+    private Date registrationDate;
 }
