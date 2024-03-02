4b3d796b917edd3ea5e13e2d1c59d17458051067
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-03 02:45:11 +0900

    [feat] 회원 엔티티 JPA 기반 어노테이션 적용

--- src/main/java/org/sparta/library/entity/User.java
+++ src/main/java/org/sparta/library/entity/User.java
@@ -1,17 +1,33 @@
 package org.sparta.library.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "user")
 @NoArgsConstructor
 @Getter
 @Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "gender", nullable = false)
     private String gender;
+
+    @Column(name = "identification_number", nullable = false, unique = true)
     private String identificationNumber;
+
+    @Column(name = "phone_number", nullable = false, unique = true)
     private String phoneNumber;
+
+    @Column(name = "address", nullable = false)
     private String address;
 }
