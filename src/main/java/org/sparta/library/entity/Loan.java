7e4476b4497f3db45dfafe05b1d66778fe385fcc
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-02 23:00:39 +0900

    [feat] 대출 엔티티 필드 선언

--- src/main/java/org/sparta/library/entity/Loan.java
+++ src/main/java/org/sparta/library/entity/Loan.java
@@ -1,4 +1,19 @@
 package org.sparta.library.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Loan {
+    private Long loanId;
+    private Long userId;
+    private Long bookId;
+    private Boolean bookReturn;
+    private Date loanDate;
+    private Date returnDate;
 }
