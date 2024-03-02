1957ac4b5dc434d2323df62bbb598e59fe657b3d
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-02 22:53:17 +0900

    [feat] 회원 엔티티 필드 선언

--- src/main/java/org/sparta/library/entity/User.java
+++ src/main/java/org/sparta/library/entity/User.java
@@ -1,4 +1,16 @@
 package org.sparta.library.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    private Long userId;
+    private String name;
+    private String gender;
+    private String identificationNumber;
+    private String address;
 }
