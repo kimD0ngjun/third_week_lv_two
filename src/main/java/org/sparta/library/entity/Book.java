103e4bee9d6947b8759c348f824e2458b2e58fb1
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-03 02:38:33 +0900

    [feat] 도서 엔티티 JPA 기반 어노테이션 적용

--- src/main/java/org/sparta/library/entity/Book.java
+++ src/main/java/org/sparta/library/entity/Book.java
@@ -1,19 +1,35 @@
 package org.sparta.library.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
+@Entity
+@Table(name = "book")
 @NoArgsConstructor
 @Getter
 @Setter
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
     private Long bookId;
+
+    @Column(name = "title", nullable = false)
     private String title;
+
+    @Column(name = "writer", nullable = false)
     private String writer;
+
+    @Column(name = "language", nullable = false)
     private String language;
+
+    @Column(name = "publisher", nullable = false)
     private String publisher;
+
+    @Column(name = "registration_date", nullable = false)
     private Date registrationDate;
 }
