2ed172781009d78e55101c260eab751250ca6258
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-03 20:26:10 +0900

    [refactoring] 대출 response dto 수정

--- src/main/java/org/sparta/library/dto/loanDto/LoanResponseDto.java
+++ src/main/java/org/sparta/library/dto/loanDto/LoanResponseDto.java
@@ -5,14 +5,12 @@ import org.sparta.library.entity.Loan;
 
 @Getter
 public class LoanResponseDto {
-    private Long loanId;
     private String name;
     private String phoneNumber;
     private String title;
     private String writer;
 
     public LoanResponseDto(Loan loan) {
-        this.loanId = loan.getLoanId();
         this.name = loan.getUser().getName();
         this.phoneNumber = loan.getUser().getPhoneNumber();
         this.title = loan.getBook().getTitle();
