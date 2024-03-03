ee7b1c849fa8a06ca32452ce7ab7015632cbd4b3
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-03 17:44:00 +0900

    [feat] Loan 서비스 구현체 메소드 프레임

--- src/main/java/org/sparta/library/service/loanservice/LoanServiceImpl.java
+++ src/main/java/org/sparta/library/service/loanservice/LoanServiceImpl.java
@@ -1,22 +1,32 @@
 package org.sparta.library.service.loanservice;
 
+import jakarta.transaction.Transactional;
 import org.sparta.library.dto.loanDto.LoanResponseDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public interface LoanService {
+public class LoanServiceImpl implements LoanService {
     // 객체 생성(도서 대출)
     //TODO: 객체를 생성하려면, 전달받은 도서 ID를 기반으로 대출 DB에서 조회한다
     // 1. 도서(들)가 조회되지 않으면, 생성 가능
     // 2. 도서(들)가 조회됐지만, 반납 여부가 true이면 생성 가능
-    ResponseEntity<String> createLoan(Long bookId, Long userId);
+    @Override
+    @Transactional
+    public ResponseEntity<String> createLoan(Long bookId, Long userId) {
+        return null;
+    }
 
     // 객체 수정(도서 반납)
-    ResponseEntity<String> updateLoan(Long bookId, Long userId);
+    @Override
+    @Transactional
+    public ResponseEntity<String> updateLoan(Long bookId, Long userId) {
+        return null;
+    }
 
     // 조건부 조회(회원 ID 외래키 기반)
-    List<LoanResponseDto> getLoans(Long userId);
+    @Override
+    public List<LoanResponseDto> getLoans(Long userId) {
+        return null;
+    }
 }
